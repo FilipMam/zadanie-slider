080af2957b66bb24625b5c007af9694ec38fb69d
filipMaM <filip.mamcarczyk@gmail.com>
2021-03-17 23:00:08 +0100

    Rozwiazanie

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-const arrowLeft = document.querySelector(".arrow--light");
+const arrowLeft = document.querySelector(".arrow--left");
 const arrowRight = document.querySelector(".arrow--right");
+const images = document.querySelectorAll("img");
 
-const currentSlide = 0;
+let currentSlide;
 
 const showSlide = (slideIndex) => {
-    [...document.querySelectorAll("img")][slideIndex].classList.add("active");
+    document.querySelector(".active") && document.querySelector(".active").classList.remove("active");
+    [...images][slideIndex].classList.add("active");
+    currentSlide = slideIndex;
 };
 
-// Tutaj umieść swój kod.
-// Po kliknięciu na strzałkę w prawo powinien się wyświetlić następny slide.
-// Po kliknięciu na strzałkę w lewo powinien się wyświetlić porpzedni slide.
-// Powodzenia!
+
+arrowLeft.addEventListener("click", () => {
+    let nextSlide;
+    if (currentSlide === 0) {
+        nextSlide = images.length - 1;
+    } else {
+        nextSlide = currentSlide - 1;
+    }
+    showSlide(nextSlide);
+});
+
+arrowRight.addEventListener("click", () => {
+    let nextSlide;
+    if (currentSlide === images.length - 1) {
+        nextSlide = 0;
+    } else {
+        nextSlide = currentSlide + 1;
+    }
+    showSlide(nextSlide);
+});
 
 showSlide(1);
