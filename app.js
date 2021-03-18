5b9571283754571031298d2388ce4cff8fd702c6
patrickw7 <76102735+patrickw7@users.noreply.github.com>
2021-03-18 19:02:19 +0100

    Add files via upload
    solution

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const arrowLeft = document.querySelector(".arrow--light");
+const arrowLeft = document.querySelector(".arrow--left");
 const arrowRight = document.querySelector(".arrow--right");
 const images = document.querySelectorAll("img");
 
-const currentSlide;
+let currentSlide = 0;
 
 const showSlide = (slideIndex) => {
     document.querySelector(".active") && document.querySelector(".active").classList.remove("active");
@@ -10,9 +10,32 @@ const showSlide = (slideIndex) => {
     currentSlide = slideIndex;
 };
 
+arrowRight.addEventListener("click", function() {
+    
+        if (currentSlide == images.length - 1) {
+            currentSlide = 0;
+        } else {
+            currentSlide++;
+        }
+        showSlide(currentSlide);
+})
+
+arrowLeft.addEventListener("click", function() {
+    if (currentSlide == 0) {
+        currentSlide = images.length -1
+    } else {
+        currentSlide--;
+    }
+    showSlide(currentSlide);
+})
+
+
+ 
+
+
 // Tutaj umieść swój kod.
 // Po kliknięciu na strzałkę w prawo powinien się wyświetlić następny slide.
 // Po kliknięciu na strzałkę w lewo powinien się wyświetlić porpzedni slide.
 // Powodzenia!
 
-showSlide(1);
+showSlide(0);
